29cb9823c43ee04a1ad057fd592ccbdec86923a8
Georgiy Sobolev <goshasobolev2003@gmail.com>
2024-06-13 12:33:40 +0300

    task5_fucntions is done (использовал фунуцию-генератор)

---
+++ Module_1/task5_functions.js
@@ -0,0 +1,27 @@
+function* generateRandomId(numElements) { //функция генератор
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < numElements; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        yield characters[randomIndex];
+    }
+}
+
+function totalString(numElements){
+    let generator = generateRandomId(numElements);
+    let result = ''
+    while(true){
+        let { value, done } = generator.next();
+        if (done){
+            break;
+        }
+        result += value
+    }
+    return result
+}
+
+console.log(totalString(10))
+
+
+
+
+
